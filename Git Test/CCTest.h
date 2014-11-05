04ecf169a0811cd0a77355ed0cc8646aa6d2e414
Sunrai <bryheath86@gmail.com>
2014-11-05 10:22:28 -0600

    Added a Test Class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Bryan Heath on 11/5/14.
+//  Copyright (c) 2014 Bryan Heath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Bryan Heath on 11/5/14.
+//  Copyright (c) 2014 Bryan Heath. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
